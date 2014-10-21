09b9a3a62bb681d10a5de7adc4d8a3b0b4ae3a2b
francois <f_roseberry@live.fr>
2014-10-21 08:47:55 -0400

    Ajout de 2 tests pour GameGrid (qui imitent 2 cas testés sur Grid)

---
+++ src/test/java/minesweeper/form/GameGridTest.java
@@ -0,0 +1,51 @@
+package minesweeper.form;
+
+import static junit.framework.Assert.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import java.awt.event.MouseEvent;
+
+import minesweeper.model.GridSize;
+import minesweeper.model.SquareButtonState;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameGridTest {
+
+	private static final GridSize GRID_SIZE_1X1 = GridSize.create(1, 1);
+	private GameGrid grid;
+	private SquareButtonProvider providerMock;
+
+	@Before
+	public void setup() {
+		providerMock = mock(SquareButtonProvider.class);
+		GameServices gameServicesMock = mock(GameServices.class);
+		when(gameServicesMock.isInGame()).thenReturn(true);
+		grid = new GameGrid(gameServicesMock, providerMock);
+	}
+
+	@Test
+	public void atGameStartAllButtonsShouldBeHidden() {
+		SquareButton button = new SquareButton(0, 0);
+		when(providerMock.create(anyInt(), anyInt())).thenReturn(button);
+		grid.startGame(GRID_SIZE_1X1, 0);
+
+		assertEquals(SquareButtonState.HIDDEN, button.getState());
+	}
+
+	@Test
+	public void afterRevealingSquareItsStateShouldBeRevealed() {
+		SquareButton button = new SquareButton(0, 0);
+		when(providerMock.create(anyInt(), anyInt())).thenReturn(button);
+		grid.startGame(GRID_SIZE_1X1, 0);
+
+		MouseEvent eventMock = mock(MouseEvent.class);
+		when(eventMock.getSource()).thenReturn(button);
+		when(eventMock.getButton()).thenReturn(MouseEvent.BUTTON1);
+		grid.mouseReleased(eventMock);
+
+		assertEquals(SquareButtonState.REVEALED, button.getState());
+	}
+}
