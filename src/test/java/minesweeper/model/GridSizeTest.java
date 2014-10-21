cbab2e6d801356bc6a00385bc26b74e20750ad8f
Frank <f_roseberry@live.fr>
2014-10-20 21:14:34 -0400

    Ajout d un test poue GridSize

--- src/test/java/minesweeper/model/GridSizeTest.java
+++ src/test/java/minesweeper/model/GridSizeTest.java
@@ -1,5 +1,7 @@
 package minesweeper.model;
 
+import static junit.framework.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class GridSizeTest {
@@ -12,5 +14,13 @@ public class GridSizeTest {
 	@Test(expected = IllegalArgumentException.class)
 	public void creatingWithZeroOrLessColumnsShouldThrowException() {
 		GridSize.create(1, 0);
+	}
+
+	@Test
+	public void cellCountShouldBeRowTimesColumns() {
+		int rows = 3;
+		int columns = 4;
+		GridSize size = GridSize.create(rows, columns);
+		assertEquals(rows * columns, size.cellCount());
 	}
 }
